098ab2191d2bfe43a754c7ac754dc1ee28e00c9c
valeriyarv07 <103383160+valeriyarv07@users.noreply.github.com>
2022-11-20 19:55:50 +0200

    added open soursed link

--- src/App.js
+++ src/App.js
@@ -12,6 +12,24 @@ root.render(
     <h1>Weather</h1>
     <div className="Weather">
       <Weather />
+      <footer>
+				This project was coded by{" "}
+				<a
+					href="https://marvelous-crostata-b8703b.netlify.app"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Valeriia Rybakova
+				</a>{" "}
+				and is {""}
+				<a
+					href="https://github.com/valeriyarv07/weather-react-4-week"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					open sourced on GitHub.
+				</a>
+			</footer>
     </div>
   </StrictMode>
 );
